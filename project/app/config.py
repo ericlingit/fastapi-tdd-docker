dc5024055262cd6fd3f719208d23a1f4a88b4dab
Eric Lin <ericlin@softnext.com.tw>
2021-04-01 16:07:17 +0900

    Setup docker containers for app and db

--- project/app/config.py
+++ project/app/config.py
@@ -1,7 +1,9 @@
 import logging
 import os
-from pydantic import BaseSettings
 from functools import lru_cache
+
+from pydantic import BaseSettings, AnyUrl
+
 
 log = logging.getLogger('uvicorn')
 
@@ -9,9 +11,10 @@ log = logging.getLogger('uvicorn')
 class Settings(BaseSettings):
     environment: str = os.getenv('ENVIRONMENT', 'dev')
     testing: bool = os.getenv('TESTING', 0)
+    database_url: AnyUrl = os.environ.get('DATABASE_URL')
 
 
-@lru_cache
+@lru_cache()
 def get_settings() -> Settings:
     log.info('Loading config settings from env')
     return Settings()
