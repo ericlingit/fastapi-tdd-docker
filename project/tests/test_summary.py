2d601671f87fb40d8695be79cb480d0c346fae56
ericlingit <ericlingit@users.noreply.github.com>
2021-04-07 16:29:05 +0900

    Add PUT route arg validation test

--- project/tests/test_summary.py
+++ project/tests/test_summary.py
@@ -121,6 +121,22 @@ def test_update_summary_bad_id(test_app_with_db: TestClient):
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
+    response = test_app_with_db.put(
+        "/summary/0",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated"})
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["path", "id"],
+                "msg": "ensure this value is greater than 0",
+                "type": "value_error.number.not_gt",
+                "ctx": {"limit_value": 0},
+            }
+        ]
+    }
+
 
 def test_update_summary_bad_json(test_app_with_db: TestClient):
     response = test_app_with_db.post(
